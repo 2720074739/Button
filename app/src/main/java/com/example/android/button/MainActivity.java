b6cb9cb4e32994e526c3a2dfc142ca2a63d1d5fe
2720074739 <2720074739z@gmail.com>
2017-09-11 15:54:50 +1000

    two

---
+++ app/src/main/java/com/example/android/button/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.android.button;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void sendScreen(View view) {
+        EditText editText = (EditText)findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+
+    }
+}
